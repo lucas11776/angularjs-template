cd4e6208800272310091e34b7beddeba2960ede6
Themba <thembangubeni04@gmail.com>
2019-04-07 15:45:03 -0700

    Uploaded file.

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="../jquery/jquery-3.3.1.min.js" />
+
+//open navbar
+$scope.open = function () {
+    if ($('#sidenav').width() == 0) {
+        $('#sidenav').width('250px');
+    } else {
+        $('#sidenav').width('0');
+    }
+};
+
+//close nabar
+$scope.close = function () {
+    $('#sidenav').width('0');
+};
+
+
+(function () {
+    // toggle navbar
+    $('.navbar-btn').click(function () {
+        if($('#sidenav').width() == 0){
+
+        }else{}
+    });
+}());
